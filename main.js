bee16293b006d41abe90ce3267f4c019ce744748
Miguel Ardila <miguel_sxe@hotmail.com>
2024-12-03 12:48:41 -0500

    visible, hidden git

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 let noButton = document.getElementById("no-button");
 let yesButton = document.getElementById("yes-button");
 let message = document.getElementById("message");
+let messageText = document.getElementById("message-text");
+let pigGif = document.getElementById("pig-gif");
 
 let hoverAttempts = 0;
 
 noButton.addEventListener("mouseover", () => {
     hoverAttempts++;
-    if (hoverAttempts < 20) {
-        // Mover el botón "No" a una posición aleatoria
-        let randomX = Math.random() * 300 - 150; // Ajustar el rango para evitar que salga de la pantalla
+    if (hoverAttempts < 7) {
+        let randomX = Math.random() * 300 - 150;
         let randomY = Math.random() * 300 - 150;
-
         noButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
     } else {
-        // Mostrar el mensaje automáticamente al cuarto intento
+        // Mostrar el mensaje y el GIF después de 7 intentos
         message.classList.remove("hidden");
+        pigGif.classList.add("visible");
     }
 });
 
 yesButton.addEventListener("click", () => {
-    // Mostrar el mensaje final
     message.classList.remove("hidden");
+    pigGif.classList.add("visible");
 });
